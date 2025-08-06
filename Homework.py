b823c5e29136e96f4c39a65a4691cef658608f32
saad09092012 <msaadatif@gmail.com>
2025-08-06 14:49:33 +0400

    Homework

---
+++ Homework.py
@@ -0,0 +1,5 @@
+age = int(input("how old are you?:"))
+if age > 10 and age < 20:
+    print("you are allowed in class age limit in between 10 and 20")
+elif age < 10 or age > 20:
+    print("you are not allowed in class")
